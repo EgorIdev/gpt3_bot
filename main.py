65d1961a2bffdc88b2630cac5559e861526a2c49
EgorIdev <117537859+EgorIdev@users.noreply.github.com>
2023-03-13 00:38:29 +0700

    just save

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import openai
 import telebot
 
-openai.api_key = "past_token"
-bot = telebot.TeleBot("past_token")
+openai.api_key = "token"
+bot = telebot.TeleBot("token")
 
 @bot.message_handler(func=lambda _: True)
 def handle_message(message):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=message.text,
-        temperature=0.5,
-        max_tokens=1000,
+        temperature=0.9,
+        max_tokens=3000,
         top_p=1.0,
-        frequency_penalty=0.5,
-        presence_penalty=0.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=["You: "]
     )
     bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
 
